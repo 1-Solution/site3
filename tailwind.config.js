4ccdfbf3896118ad52a6a373362b90723833475a
Jean-Christophe Viau <jean-christophe@1-solution.ca>
2025-06-09 18:18:23 -0400

    chore: update dependencies and add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths based on your project structure
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#0C2830',
+        'brand-orange': '#42B5C5',
+      },
+      backgroundImage: {
+      }
+    }
+  }
+}
