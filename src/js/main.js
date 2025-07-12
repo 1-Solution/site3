24e3d7192d788b49de11eefb9bea9ad93e7d057e
Jean-Christophe Viau <jean-christophe@1-solution.ca>
2025-07-12 16:23:51 -0400

    partial i18n

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
 import './contact';
 import './mobile-menu';
+import i18next from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    lng: 'fr', 
+    fallbackLng: 'fr',
+    supportedLngs: ['fr','en'],
+    debug: true,
+    ns: ['about', 'hero', 'menu', 'features', 'optimize', 'partners', 'team', 'careers', 'logos', 'contact'],
+    defaultNS: 'hero',
+    interpolation: { escapeValue: false },
+    backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
+  })
+  .then(() => {
+    document.querySelectorAll('[data-i18n]').forEach(el => {
+      const key = el.getAttribute('data-i18n')
+      el.innerHTML = i18next.t(key)
+    })
+  })
+
+document.getElementById('lang-switcher')
+  .addEventListener('change', e => {
+    i18next.changeLanguage(e.target.value)
+      .then(() => {
+        document.querySelectorAll('[data-i18n]').forEach(el => {
+          el.innerHTML = i18next.t(el.dataset.i18n)
+        })
+      })
+  })
